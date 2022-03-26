cd1a13958e08e87e727e0d4165b86a8db8bd7f7f
zilurahman10 <raihan.brac2019@gmail.com>
2022-03-26 23:14:30 +0600

    final-commit

--- src/components/SingleItem/SingleItem.js
+++ src/components/SingleItem/SingleItem.js
@@ -4,7 +4,7 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './SingleItem.css'
 
 const SingleItem = (props) => {
-    const { name, img, price } = props.product
+    const { name, img, price, id } = props.product
     const { addToCart } = props
     return (
         <div>
@@ -15,6 +15,7 @@ const SingleItem = (props) => {
                 <div className='text'>
                     <h2>{name}</h2>
                     <h4>Price : $ {price}</h4>
+                    <p>Id : {id}</p>
                 </div>
                 <div className='btn'>
                     <button onClick={() => addToCart(props.product)} className='btn'>Add to cart
