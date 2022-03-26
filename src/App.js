ebad72fa86d9a0ea95cf97bf66e1dfef9c348b86
zillurahman10 <raihan.brac2019@gmail.com>
2022-03-26 20:40:05 +0600

    Question-component-added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Items from './components/Items/Items';
+import Question from './components/Question/Question';
 
 function App() {
   return (
     <div className="App">
       <Items></Items>
+      <div>
+        <Question></Question>
+      </div>
     </div>
   );
 }
